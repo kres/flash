7c6654b9d5f823cc31b764237e44fa7945f5d6fe
deepakkarki <deepak6k@gmail.com>
2014-11-19 13:51:10 +0530

    modified http client to match server

--- epoll-http-static-server/test_client.py
+++ epoll-http-static-server/test_client.py
@@ -3,18 +3,16 @@ import socket
 TCP_IP = '127.0.0.1'
 TCP_PORT = 6060
 BUFFER_SIZE = 1024
-MESSAGE = "ab"
+MESSAGE = '''GET /foo/bar HTTP/1.1
+Host: api.opencalais.com
+Content-Type: text/xml; charset=utf-8'''
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send('x')
-import time
-time.sleep(3)
-s.send('y')
+s.send(MESSAGE)
 data = s.recv(BUFFER_SIZE)
-print "received data:", data
-while True:
-	pass
+print "received data : "
+print data
 s.close()
 
 
